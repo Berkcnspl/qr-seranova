e04a5d997cb3f9d5082441b21512dac9c902a9c9
Berkcnspl <sipal.berkcan@gmail.com>
2025-06-26 19:50:53 +0300

    Layout.tsx güncellendi.

--- app/batch/[id]/layout.tsx
+++ app/batch/[id]/layout.tsx
@@ -3,5 +3,19 @@ export default function BatchIdLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <body
+      style={{
+        margin: 0,
+        padding: 0,
+        backgroundColor: "#111",
+        minHeight: "100vh",
+        width: "100vw",
+        color: "#fff",
+        fontFamily: "Inter, sans-serif",
+      }}
+    >
+      {children}
+    </body>
+  );
 }
